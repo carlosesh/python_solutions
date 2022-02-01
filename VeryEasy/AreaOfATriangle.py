4c0f69e10494fb8c7c8b08063708fa3cdb75bc4a
Carlos Sanchez <carlosesh@outlook.com>
2022-01-31 20:07:30 -0600

    now, the classes follow the structure of a unit test

--- VeryEasy/AreaOfATriangle.py
+++ VeryEasy/AreaOfATriangle.py
@@ -1,2 +1,36 @@
+import unittest
+
+"""
+Area of a Triangle
+Write a function that takes the base and height of a triangle
+and return its area.
+
+Examples
+tri_area(3, 2) ➞ 3
+
+tri_area(7, 4) ➞ 14
+
+tri_area(10, 10) ➞ 50
+
+Notes
+* The area of a triangle is: (base * height) / 2
+* Don't forget to return the result.
+* If you get stuck on a challenge, find help in the Resources tab.
+* If you're really stuck, unlock solutions in the Solutions tab.
+"""
+
 def tri_area(base, height):
 	return (base * height) / 2
+
+
+class AreaOfATriangle(unittest.TestCase):
+	def test_tri_area(self):
+		self.assertEqual(tri_area(3, 2), 3)
+		self.assertEqual(tri_area(5, 4), 10)
+		self.assertEqual(tri_area(10, 10), 50)
+		self.assertEqual(tri_area(0, 60), 0)
+		self.assertEqual(tri_area(12, 11), 66)
+
+
+if __name__ == '__main__':
+	unittest.main()
--- VeryEasy/LengthOfNumber.py
+++ VeryEasy/LengthOfNumber.py
@@ -1,18 +1,41 @@
+import unittest
+
+"""
+Length of Number
+
+Create a function that takes a number num and returns its length.
+
+Examples
+number_length(10) ➞ 2
+
+number_length(5000) ➞ 4
+
+number_length(0) ➞ 1
+
+Notes
+The use of the len() function is prohibited.
+"""
+
 def number_length(num):
 	if num == 0:
 		return 1
 	else:
 		count = 0
-		while int(num) > 0:
+		while num > 0:
 			count += 1
-			num /= 10
+			num //= 10
 		return count
 
+class LengthOfNumber(unittest.TestCase):
+	def test_number_length(self):
+		self.assertEqual(number_length(10), 2)
+		self.assertEqual(number_length(5000), 4)
+		self.assertEqual(number_length(0), 1)
+		self.assertEqual(number_length(4039182), 7)
+		self.assertEqual(number_length(9999999999999999), 16)
+		self.assertEqual(number_length(1), 1)
+		self.assertEqual(number_length(777777777777777777777777777777), 30)
 
-print(number_length(10))
-print(number_length(5000))
-print(number_length(0))
-print(number_length(4039182))
-print(number_length(9999999999999999))
-print(number_length(1))
-print(number_length(777777777777777777777777777777))
+
+if __name__ == '__main__':
+	unittest.main()
