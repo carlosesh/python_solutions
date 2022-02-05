551321a375d3cae0bec2e6ea2eb994c11399cf6a
Carlos Sanchez <carlosesh@outlook.com>
2022-02-04 22:10:11 -0600

    fix to formatting

--- codewars/7-kyu/SomeButNotAll.py
+++ codewars/7-kyu/SomeButNotAll.py
@@ -26,12 +26,14 @@ some([4, 4], lambda x: x>3)
 >>> False
 """
 
+
 def some_but_not_all(seq, pred):
     flag_list = []
     for item in seq:
         flag_list.append(pred(item))
 
     return False if all(flag_list) else any(flag_list)
+
 
 class SomeButNotAll(unittest.TestCase):
     def test_some_but_not_all(self):
@@ -41,6 +43,7 @@ class SomeButNotAll(unittest.TestCase):
         self.assertEqual(some_but_not_all([4, 1], lambda x: x > 3), True)
         self.assertEqual(some_but_not_all([1, 1], lambda x: x > 3), False)
         self.assertEqual(some_but_not_all([4, 4], lambda x: x > 3), False)
+
 
 if __name__ == '__main__':
     unittest.main()
