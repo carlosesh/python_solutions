0fe0a5fe2749ce5e3ac7dd5e392365965bb2d32e
Carlos Sanchez <carlosesh@outlook.com>
2022-02-22 23:02:01 -0600

    1133. Largest Unique Number

---
+++ leetcode/easy/1133-LargestUniqueNumber.py
@@ -0,0 +1,40 @@
+"""
+1133. Largest Unique Number
+
+https://leetcode.com/problems/largest-unique-number/
+
+Given an integer array nums, return the largest integer that only occurs once.
+If no integer occurs once, return -1.
+
+
+
+Example 1:
+Input: nums = [5,7,3,9,4,9,8,3,1]
+Output: 8
+Explanation: The maximum integer in the array is 9 but it is repeated.
+The number 8 occurs only once, so it is the answer.
+
+Example 2:
+Input: nums = [9,9,8,8]
+Output: -1
+Explanation: There is no number that occurs only once.
+
+
+Constraints:
+1 <= nums.length <= 2000
+0 <= nums[i] <= 1000
+"""
+
+
+class Solution:
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+        nums_dict = defaultdict(int)
+        for i in range(len(nums)):
+            nums_dict[nums[i]] += 1
+
+        current_max = -1
+        for key, value in nums_dict.items():
+            if value == 1:
+                current_max = key if key > current_max else current_max
+
+        return current_max
