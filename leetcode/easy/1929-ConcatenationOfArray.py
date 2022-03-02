9a48e972938d3a660ec39aa7e53d837948a41beb
Carlos <carlosesh@outlook.com>
2022-03-02 12:26:36 -0600

    1929. Concatenation of Array

---
+++ leetcode/easy/1929-ConcatenationOfArray.py
@@ -0,0 +1,8 @@
+"""
+1929. Concatenation of Array
+https://leetcode.com/problems/concatenation-of-array/
+"""
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
